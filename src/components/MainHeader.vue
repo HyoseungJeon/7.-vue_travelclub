8128ca473626ed22bba9fb36a990eeeea3fe0d63
Hyoseung Jeon <qmflwj1441@gmail.com>
2021-01-25 15:26:15 +0900

    Add TravelClub

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,25 @@
+<template>
+   <sui-grid divided="vertically">
+    <sui-grid-row :columns="16">
+      <sui-grid-column :width="3">
+			<h4 is="sui-header" color="green">TravelClub</h4>
+      </sui-grid-column>
+			<sui-grid-column :width="10">
+        
+      </sui-grid-column>
+      <sui-grid-column :width="3">
+        
+      </sui-grid-column>
+    </sui-grid-row>
+  </sui-grid>
+</template>
+
+<script>
+export default {
+    name:"MainHeader"
+}
+</script>
+
+<style>
+
+</style>
